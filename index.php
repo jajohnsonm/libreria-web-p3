c369a1ace92322cb41df82c832fa981e7b7ade48
jajohnsonm <104536232+jajohnsonm@users.noreply.github.com>
2025-04-04 21:24:37 -0400

    index and database imported into the hosting

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+    <section class="container text-center">
+        <h1 class="title">Libreria</h1>
+        <br><br>
+        <div class="links">
+        <a href="functions/libros/libros.php" class="btn btn-outline-dark">Libros Disponibles</a>
+        <a href="functions/autores/autores.php" class="btn btn-outline-dark">Autores</a>
+        <a href="functions/contacto/contacto.php" class="btn btn-outline-dark">Formulario de Contacto</a>
+
+        </div>
+     </div>    
+    </section>
+    <section class="container text-center"></section>
+</body>
+</html>
